561d0c0b06cd0bab5ff2246a6744ed0e46b136bf
Stephen Regan James Bacolod Layson <jameslayson.0@gmail.com>
2023-03-24 15:06:13 +0800

    commit

--- app/src/main/java/com/example/bcash/bcash_api.java
+++ app/src/main/java/com/example/bcash/bcash_api.java
@@ -1,6 +1,6 @@
 package com.example.bcash;
 
 public class bcash_api {
-    public final String url = "https://xnpel.localtonet.com/bcash-api";
+    public final String url = "http://4roaz.localtonet.com/bcash-api";
 
 }
